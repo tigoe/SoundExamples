681810b923c7e14876bcbb1017173156ad4d0139
tigoe <tom.igoe@gmail.com>
2018-12-15 11:33:46 -0500

    added simple MIDI players for hardware serial, SW serial, midiUSB

---
+++ MIDI_examples/MIDIUSB_simple/MIDIUSB_simple.ino
@@ -0,0 +1,59 @@
+/*
+  Simple MIDI player for SoftwareSerial
+
+  Generates a series of MIDI notes.
+   The melody is Steve Reich's "Piano phase"
+
+  Uses SoftwareSerial for MIDI, so will work on any 
+  328 or 32U4-based board (e.g. Uno, Leonardo, Micro, Yún)
+ 
+  Circuit:
+    connect TX of SoftwareSerial (pin 3) to TX pin of MIDI jack
+ 
+  created 13 Feb 2017
+  modified 13 Dec 2018
+  by Tom Igoe
+*/
+
+#include <MIDIUSB.h>
+
+
+int bpm = 72;  // beats per minute
+// duration of a beat in ms
+float beatDuration = 60.0 / bpm * 1000;   
+
+// the melody sequence:
+int melody[] = {64, 66, 71, 73, 74, 66, 64, 73, 71, 66, 74, 73};
+// which note of the melody to play:
+int thisNote = 0;
+
+void setup() {
+  
+}
+
+void loop() {
+  // play a note from the melody:
+  midiCommand(0x90, melody[thisNote], 127);
+  // all the notes in this are sixteenth notes, 
+  // which is 1/4 of a beat, so:
+  int noteDuration = beatDuration / 4;
+  // I like when the note is on 60% of the note time:
+  delay(noteDuration);
+  // turn the note off:
+  midiCommand(0x80, melody[thisNote], 127);
+  // increment the note number for next time through the loop:
+  thisNote++;
+  // keep the note in the range from 0 - 12 using modulo:
+  thisNote = thisNote % 12;
+}
+
+// send a 3-byte midi message
+void midiCommand(byte cmd, byte data1, byte  data2) {
+  // First parameter is the event type (top 4 bits of the command byte).
+  // Second parameter is command byte combined with the channel.
+  // Third parameter is the first data byte
+  // Fourth parameter second data byte
+  
+  midiEventPacket_t midiMsg = {cmd >> 4, cmd, data1, data2};
+  MidiUSB.sendMIDI(midiMsg);
+}
---
+++ MIDI_examples/Serial1_MIDI_simple/Serial1_MIDI_simple.ino
@@ -0,0 +1,55 @@
+/*
+  Simple MIDI player for Serial1
+
+  Generates a series of MIDI notes.
+   The melody is Steve Reich's "Piano phase"
+
+  Uses Serial1 for MIDI, so will work on any board
+  with 2 hardware serial ports: MKR boards, Leonardo, Micro, or Yún
+
+  Circuit:
+    connect TX of Serial1 to TX pin of MIDI jack
+ 
+  created 13 Feb 2017
+  modified 13 Dec 2018
+  by Tom Igoe
+*/
+
+int bpm = 72;  // beats per minute
+// duration of a beat in ms
+float beatDuration = 60.0 / bpm * 1000;   
+
+// the melody sequence:
+int melody[] = {64, 66, 71, 73, 74, 66, 64, 73, 71, 66, 74, 73};
+// which note of the melody to play:
+int thisNote = 0;
+
+void setup() {
+  // initialize serial:
+  Serial.begin(9600);
+  // initialize MIDI serial:
+  Serial1.begin(31250);
+}
+
+void loop() {
+  // play a note from the melody:
+  midiCommand(0x90, melody[thisNote], 127);
+  // all the notes in this are sixteenth notes, 
+  // which is 1/4 of a beat, so:
+  int noteDuration = beatDuration / 4;
+  // I like when the note is on 60% of the note time:
+  delay(noteDuration);
+  // turn the note off:
+  midiCommand(0x80, melody[thisNote], 127);
+  // increment the note number for next time through the loop:
+  thisNote++;
+  // keep the note in the range from 0 - 12 using modulo:
+  thisNote = thisNote % 12;
+}
+
+// send a 3-byte midi message
+void midiCommand(byte cmd, byte data1, byte  data2) {
+  Serial1.write(cmd);     // command byte (should be > 127)
+  Serial1.write(data1);   // data byte 1 (should be < 128)
+  Serial1.write(data2);   // data byte 2 (should be < 128)
+}
