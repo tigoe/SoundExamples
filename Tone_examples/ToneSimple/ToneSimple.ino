154fc5945d9fda5a901cc02433a7a35b8aa05513
Tom Igoe <tom.igoe@gmail.com>
2020-06-17 16:21:25 -0400

    Update ToneSimple.ino

--- Tone_examples/ToneSimple/ToneSimple.ino
+++ Tone_examples/ToneSimple/ToneSimple.ino
@@ -11,20 +11,27 @@
    by Tom Igoe
 */
 
-const int speakerPin = 5;  // the pin number for the speaker
+// middle A is the reference frequency for an
+// equal-tempered scale. Set its frequency and note value:
+#define NOTE_A4 69         // MIDI note value for middle A
+#define FREQ_A4 440        // frequency for middle A
 
+const int speakerPin = A0;  // the pin number for the speaker
 void setup() {
+  Serial.begin(9600);
   // make the speaker pin an output:
   pinMode(speakerPin, OUTPUT);
 }
-
 void loop() {
-  // read the potentiometer:
-  int sensorValue = analogRead(A0);
-  // map the potentiometer to a range
-  // from A0 to C8 (27.5 Hz to 4186):
-  float frequency = map(sensorValue, 0, 1023, 27.5, 4186);
+  // convert sensor reading to 21 - 108 range
+  // which is the range of MIDI notes on an 88-key keyboard
+  // (from A0 to C8):
+  int sensorReading = analogRead(A1);
+  int noteValue = map(sensorReading, 0, 1023, 21, 108);
+  // then convert to frequency:
+  float frequency =  FREQ_A4 * pow(2, ((noteValue - NOTE_A4) / 12.0));
   // turn the speaker on:
-  tone(speakerPin, frequency);
-  delay(30);
+  tone(speakerPin, frequency, 300);
+  Serial.println(frequency);
+  delay(500);
 }
